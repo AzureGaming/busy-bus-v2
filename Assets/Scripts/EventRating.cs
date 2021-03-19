82a246ee4d98b49ba2f58f100eb7ab17477f1b5b
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-03-18 21:33:52 -0400

    Add rating criteria

--- Assets/Scripts/EventRating.cs
+++ Assets/Scripts/EventRating.cs
@@ -10,6 +10,23 @@ public class EventRating : MonoBehaviour {
     public static FailEvent OnFailEvent;
 
     string[] grades = new string[] { "Needs Improvement", "Satisfactory", "Excellent" };
+    public string grade {
+        get {
+            if (fails >= 5) {
+                return grades[0];
+            }
+
+            if (greats > goods) {
+                return grades[2];
+            } else {
+                return grades[1];
+            }
+        }
+    }
+
+    int fails = 0; // TODO: reset to 0?
+    int goods = 0;
+    int greats = 0;
 
     private void OnEnable() {
         OnRateEvent += Rate;
@@ -33,10 +50,17 @@ public class EventRating : MonoBehaviour {
     }
 
     void RateDriving(float timeElapsed, float totalTime) {
-        Debug.LogWarning("Not implemented");
+        float ratio = timeElapsed / totalTime;
+        if (ratio >= 0.75) {
+            greats++;
+        } else if (ratio >= 0.25) {
+            goods++;
+        } else {
+            fails++;
+        }
     }
 
     public void Fail() {
-        Debug.LogWarning("Not implemented");
+        fails++;
     }
 }
