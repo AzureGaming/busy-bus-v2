c62e2632db6e2e765606d4bea2cb66b5a3238a57
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-03-27 17:20:33 -0400

    Add lane change prompting

---
+++ Assets/DrivePrompt.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrivePrompt : MonoBehaviour {
+    public delegate void LeftLaneChange();
+    public static LeftLaneChange OnLeftLaneChange;
+    public delegate void RightLaneChange();
+    public static RightLaneChange OnRightLaneChange;
+    public delegate void CompleteLaneChange();
+    public static CompleteLaneChange OnCompleteLaneChange;
+
+    Image image;
+    Color startColor;
+
+    private void Awake() {
+        image = GetComponent<Image>();
+    }
+
+    private void Start() {
+        startColor = image.color;
+    }
+
+    private void OnEnable() {
+        OnLeftLaneChange += ShowLeftPrompt;
+        OnRightLaneChange += ShowRightPrompt;
+        OnCompleteLaneChange += HidePrompt;
+    }
+
+    private void OnDisable() {
+        OnLeftLaneChange -= ShowLeftPrompt;
+        OnRightLaneChange -= ShowRightPrompt;
+        OnCompleteLaneChange -= HidePrompt;
+    }
+
+    void ShowLeftPrompt() {
+        image.color = startColor;
+        Quaternion newRot = image.transform.rotation;
+        newRot.y = 0;
+        image.transform.rotation = newRot;
+    }
+
+    void ShowRightPrompt() {
+        image.color = startColor;
+        Quaternion newRot = image.transform.rotation;
+        newRot.y = 180;
+        image.transform.rotation = newRot;
+    }
+
+    void HidePrompt() {
+        image.color = Color.clear;
+    }
+}
--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -3,38 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Background : MonoBehaviour {
+    public delegate void LeftLaneChange(bool skipAnimation);
+    public static LeftLaneChange OnLeftLaneChange;
+    public delegate void RightLaneChange(bool skipAnimation);
+    public static RightLaneChange OnRightLaneChange;
+
     public GameObject leftLanePosition;
     public GameObject rightLanePosition;
     public GameObject image;
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.A)) {
-            GoToLeftLane();
-        } else if (Input.GetKeyDown(KeyCode.D)) {
-            GoToRightLane();
-        }
+    //private void Update() {
+    //    if (Input.GetKeyDown(KeyCode.A)) {
+    //        GoToLeftLane();
+    //    } else if (Input.GetKeyDown(KeyCode.D)) {
+    //        GoToRightLane();
+    //    }
+    //}
+
+    private void OnEnable() {
+        OnLeftLaneChange += GoToLeftLane;
+        OnRightLaneChange += GoToRightLane;
     }
 
-    public void GoToLeftLane() {
-        StartCoroutine(GoToPosition(leftLanePosition.transform.position));
+    private void OnDisable() {
+        OnLeftLaneChange -= GoToLeftLane;
+        OnRightLaneChange -= GoToRightLane;
     }
 
-    public void GoToRightLane() {
-        StartCoroutine(GoToPosition(rightLanePosition.transform.position));
+    public void GoToLeftLane(bool skipAnimation) {
+        StopAllCoroutines();
+        StartCoroutine(GoToPosition(leftLanePosition.transform.position, skipAnimation));
     }
 
-    IEnumerator GoToPosition(Vector3 newPos) {
+    public void GoToRightLane(bool skipAnimation) {
+        StopAllCoroutines();
+        StartCoroutine(GoToPosition(rightLanePosition.transform.position, skipAnimation));
+    }
+
+    IEnumerator GoToPosition(Vector3 newPos, bool skipAnimation) {
         float timeElapsed = 0f;
         float totalTime = 1f;
         Vector3 pos = image.transform.position;
 
-        while (timeElapsed <= totalTime) {
-            Vector3 nextPos = image.transform.position;
-            float nextPosX = Mathf.Lerp(pos.x, newPos.x, ( timeElapsed / totalTime ));
-            nextPos.x = nextPosX;
-            image.transform.position = nextPos;
-            timeElapsed += Time.deltaTime;
-            yield return null;
+        if (!skipAnimation) {
+            while (timeElapsed <= totalTime) {
+                Vector3 nextPos = image.transform.position;
+                float nextPosX = Mathf.Lerp(pos.x, newPos.x, ( timeElapsed / totalTime ));
+                nextPos.x = nextPosX;
+                image.transform.position = nextPos;
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
         }
         image.transform.position = newPos;
     }
