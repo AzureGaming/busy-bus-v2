e138730b03491710ff46f0a034f1437492c38bfc
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-05-30 17:48:39 -0400

    Add render disable instead of gameobject

--- Assets/Scripts/DisplayManager.cs
+++ Assets/Scripts/DisplayManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DisplayManager : MonoBehaviour {
     public delegate void LookForward();
@@ -28,18 +29,34 @@ public class DisplayManager : MonoBehaviour {
     }
 
     void DisplayFrontOfBus() {
-        city.SetActive(true);
-        bus.SetActive(true);
-        backOfBus.SetActive(false);
+        Show(city);
+        Show(bus);
+        Hide(backOfBus);
     }
 
     void DisplayBackOfBus() {
-        city.SetActive(true);
-        bus.SetActive(false);
-        backOfBus.SetActive(true);
+        Show(city);
+        Hide(bus);
+        Show(backOfBus);
     }
 
     void DisplayPassengerMenu() {
         Debug.LogWarning("TODO: IMPLEMENT PASSENGER MENU");
+    }
+
+    // Ref: https://answers.unity.com/questions/840927/how-do-i-disable-a-renderer-on-a-ui-object-in-46.html 
+    void Show(GameObject obj) {
+        UIBehaviour[] renderers = obj.GetComponentsInChildren<UIBehaviour>();
+        foreach (UIBehaviour renderer in renderers) {
+            renderer.enabled = true;
+        }
+    }
+
+    // Ref: https://answers.unity.com/questions/840927/how-do-i-disable-a-renderer-on-a-ui-object-in-46.html 
+    void Hide(GameObject obj) {
+        UIBehaviour[] renderers = obj.GetComponentsInChildren<UIBehaviour>();
+        foreach (UIBehaviour renderer in renderers) {
+            renderer.enabled = false;
+        }
     }
 }
